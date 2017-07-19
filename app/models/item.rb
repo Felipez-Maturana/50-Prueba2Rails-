14cda55a3179341db22df7abb582df4630021014
Felipez-Maturana <felipe.maturana.g@usach.cl>
2017-07-19 19:49:04 -0400

    6 commit, borrado: when u delete an user their items are deleted too. when you erase the last item of a category, it erase the category too

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,15 @@
 class Item < ApplicationRecord
 	scope :size,-> w {where(size: w) }
 	scope :nouser,-> {where(user_id: nil)}
-  belongs_to :category
-  belongs_to :user, optional: true
+	after_destroy :lastitem
+	belongs_to :category
+	belongs_to :user, optional: true
+
+
+  def lastitem
+  	if(Item.where(category_id: self.category_id).length == 0  )
+  		Category.where(id: self.category_id)[0].destroy
+  	end
+  end
+
 end
